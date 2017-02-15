6a7d612ef83c032e387ff514e3a461c540fad87d
Yan Zhai <yanzhai@cs.wisc.edu>
2017-02-14 23:12:24 -0600

    add examples for programming

---
+++ examples/libnl/ipshow.c
@@ -0,0 +1,73 @@
+#define _GNU_SOURCE             /* See feature_test_macros(7) */
+#include <sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <netlink/socket.h>
+#include <netlink/route/rtnl.h>
+#include <netlink/route/link.h>
+
+//// This code illustrates how to program with libnl3:
+//
+//  Pitfall: Don't forget to do nl_connect after allocate a socket, or you get BAD_SOCKET error
+//
+//  the rest just follow the API reference
+
+void dump_info() {
+    struct nl_sock* sock = nl_socket_alloc();
+    if (!sock) {
+        perror("allocatenl");
+        exit(1);
+    }
+    nl_connect(sock, NETLINK_ROUTE);
+
+    struct nl_cache *cache = NULL;
+    int ret = rtnl_link_alloc_cache(sock, AF_UNSPEC, &cache);
+    if (ret < 0) {
+        printf("%p %p\n", cache, sock);
+        nl_perror(ret, "alloc cache");
+        exit(1);
+    }
+
+    int nitem = nl_cache_nitems(cache);
+    printf("there are %d items in cache\n", nitem);
+
+    struct nl_object* obj = nl_cache_get_first(cache);
+    while (obj) {
+        struct rtnl_link* l = (struct rtnl_link*) obj;
+        /// things are included in netlink-private/types.h, which are not included
+        //printf("link name %s\n", l->l_name);
+        printf("link name %s, index %d\n", rtnl_link_get_name(l), rtnl_link_get_ifindex(l));
+        obj = nl_cache_get_next(obj);
+    }
+    nl_cache_put(cache);
+}
+
+
+
+int main(int argc, char **argv) {
+
+    if (argc < 2) {
+        printf("usage: ipshow docker-netns...\n");
+        return 1;
+    }
+
+    int i ;
+    for (i = 1; i < argc; i++) {
+        int fd = open(argv[i], O_RDONLY);
+        if (fd < 0) {
+            perror("open");
+            return 2;
+        }
+        int err = setns(fd, CLONE_NEWNET);
+        if (err < 0) {
+            perror("error in joining");
+            return 2;
+        }
+        dump_info();
+    }
+
+    return 0;
+}
