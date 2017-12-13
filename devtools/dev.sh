fdd4c2e2d2a00a94da7f2546f6979e9a1323b427
Yan Zhai <zhaiyan920@gmail.com>
2017-12-13 09:57:25 -0600

    fix no python support in default vim

--- devtools/dev.sh
+++ devtools/dev.sh
@@ -8,6 +8,9 @@ export GOPATH=$HOME/go
 export GOROOT=$HOME/goroot
 export PATH=$PATH:$HOME/bin:$GOPATH/bin:$GOROOT/bin/
 
+
+GO_VERSION=1.9.2
+
 update_repo()
 {
   sudo apt-get update
@@ -16,9 +19,10 @@ update_repo()
 configure_go()
 {
   mkdir -p $HOME/go $HOME/goroot
-  wget https://redirector.gvt1.com/edgedl/go/go1.9.2.linux-amd64.tar.gz
-  tar xf go1.9.2.linux-amd64.tar.gz -C $HOME/goroot
+  wget https://redirector.gvt1.com/edgedl/go/go$GO_VERSION.linux-amd64.tar.gz
+  tar xf go$GO_VERSION.linux-amd64.tar.gz -C $HOME/goroot
   mv $HOME/goroot/go/* $HOME/goroot/
+  rm -f go$GO_VERSION.linux-amd64.tar.gz
   cp $WORKDIR/go/bashrc ~/.bashrc
   # install go tools
   go get -u github.com/nsf/gocode
@@ -39,6 +43,36 @@ install_base()
 
 configure_vim()
 {
+
+  sudo apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
+    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git libpq-dev python-tox libffi-dev libxslt1-dev
+
+  sudo apt-get install -y cmake
+
+  mkdir -p $HOME/tmp
+  cd $HOME/tmp
+  git clone https://github.com/vim/vim.git
+  cd vim
+  git checkout v7.4.2367 -b v74
+  ./configure --with-features=huge \
+    --enable-multibyte \
+    --enable-rubyinterp \
+    --enable-pythoninterp \
+    --enable-python3interp \
+    --enable-perlinterp \
+    --enable-luainterp \
+    --enable-gui=gtk2 --enable-cscope --prefix=/usr
+  #make VIMRUNTIMEDIR=/usr/share/vim/vim80
+  make -j 5
+  make install
+  sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
+  sudo update-alternatives --set editor /usr/bin/vim
+  sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
+  sudo update-alternatives --set vi /usr/bin/vim
+  cd $WORKDIR
+
   mkdir -p ~/.vim/bundle
   if ! [ -d ~/.vim/bundle/Vundle.vim ] ; then
     git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
