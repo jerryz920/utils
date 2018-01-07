f2b373abe2e02d5d178ca76cb1294015b643f4dd
Yan Zhai <zhaiyan920@gmail.com>
2018-01-07 17:48:58 -0600

    utility quick commit

--- library/cxx/install_lib.sh
+++ library/cxx/install_lib.sh
@@ -4,22 +4,27 @@ path=`dirname $exe`
 
 cd $path
 
-recursive_copy_h() {
-  local d="$1"
-  local dest="$2"
-  for f in `ls $d`; do
-    if [ -d $d/$f ]; then
-      echo mkdir -p $dest/$d/$f
-      recursive_copy_h "$d/$f" "$dest"
-    elif [ -f "$d/$f" ] && [[ $f = *.h ]]; then
-      echo cp "$d/$f"  "$dest/$d"
-    fi
-  done
-}
+#recursive_copy_h() {
+#  local d="$1"
+#  local dest="$2"
+#  for f in `ls $d`; do
+#    if [ -d $d/$f ]; then
+#      echo mkdir -p $dest/$d/$f
+#      recursive_copy_h "$d/$f" "$dest"
+#    elif [ -f "$d/$f" ] && [[ $f = *.h ]]; then
+#      echo cp "$d/$f"  "$dest/$d"
+#    fi
+#  done
+#}
+#
+## install utils
+#mkdir -p $HOME/include/jutils
+#mkdir -p $HOME/lib
+#recursive_copy_h include $HOME/include/jutils
 
-# install utils
-mkdir -p $HOME/include/jutils
-mkdir -p $HOME/lib
-recursive_copy_h include $HOME/include/jutils
 
-
+mkdir build
+cd build
+cmake ..
+make -j 4
+sudo make install
