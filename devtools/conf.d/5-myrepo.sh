cab263bbd02db3a5a9c4dc7919ad4fb9eedc7b50
Yan Zhai <zhaiyan920@gmail.com>
2018-03-05 16:31:22 -0600

    utility quick commit

--- devtools/conf.d/5-myrepo.sh
+++ devtools/conf.d/5-myrepo.sh
@@ -6,7 +6,7 @@ modify_origin()
   git remote add origin https://github.com/jerryz920/$1
   git remote add fork https://github.com/jerryz920/$1
   git fetch origin
-  git checkout -b dev --track remotes/origin/dev
+  git checkout -b ${2:-dev} --track remotes/origin/${2:-dev}
 }
 # setup kubernetes
 go get github.com/kubernetes/kubernetes
@@ -23,15 +23,14 @@ cd $GOPATH/src/github.com/docker/docker
 modify_origin docker
 # setup docker-machine
 go get github.com/docker/machine
-cd $GOPATH/src/github.com/docker/machine
-modify_origin machine
-
 go get github.com/jerryz920/linux
 go get github.com/jerryz920/boot2docker
+cd $GOPATH/src/github.com/jerryz920/boot2docker
 git checkout -b dev --track remotes/origin/dev
 go get github.com/jerryz920/utils
 go get github.com/jerryz920/hadoop
-git checkout -b dev --track remotes/origin/tapcon
+cd $GOPATH/src/github.com/jerryz920/hadoop
+git checkout -b tapcon --track remotes/origin/tapcon
 go get github.com/jerryz920/libport
 
 sudo $GOPATH/src/github.com/jerryz920/utils/library/install_lib.sh
