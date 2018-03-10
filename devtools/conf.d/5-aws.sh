40b7a96d9cc43a19d48e434060ac9ef29c5877bc
Yan Zhai <zhaiyan920@gmail.com>
2018-03-09 23:59:46 -0600

    fix dir path

--- devtools/conf.d/5-aws.sh
+++ devtools/conf.d/5-aws.sh
@@ -5,3 +5,8 @@ pip install awscli --upgrade --user
 
 find $HOME -name aws_bash_completer -exec sudo cp {} /etc/bash_completion.d/ \;
 
+wget https://s3.amazonaws.com/ec2-downloads/ec2-ami-tools.zip
+sudo unzip ec2-ami-tools.zip -d /usr/local/
+sudo mv /usr/local/ec2-ami* /usr/local/ec2
+export EC2_AMITOOL_HOME=/usr/local/ec2/
+export PATH=$EC2_AMITOOL_HOME/bin:$PATH
