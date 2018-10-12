0a50a4e8859165f2bc38025d18912b12523483ca
Yan Zhai <zhaiyan920@gmail.com>
2018-10-11 22:40:16 -0500

    fix fucking bug

--- devtools/docker/conf.d/1-go.sh
+++ devtools/docker/conf.d/1-go.sh
@@ -4,10 +4,12 @@ mkdir -p $GOROOT
 wget https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz
 tar xf go$GO_VERSION.linux-amd64.tar.gz -C $GOROOT
 
+# PATH might be overwritten even if we pass -E in sudo...
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 mv $GOROOT/go/* $GOROOT/
 ln -s $GOROOT $HOME/goroot
 rm -f go$GO_VERSION.linux-amd64.tar.gz
-cp $WORKDIR/general/bashrc ~/.bashrc
+cp $WORKDIR/docker/general/bashrc ~/.bashrc
 # install go tools
 go get -u github.com/nsf/gocode
 gocode set propose-builtins true
