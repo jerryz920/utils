d54cf8c1b87ef2cb5174e926c4e22a0efc225cbd
Yan Zhai <zhaiyan920@gmail.com>
2018-10-11 22:34:18 -0500

    fix env

--- devtools/setup.sh
+++ devtools/setup.sh
@@ -3,7 +3,7 @@
 source docker/env.sh $1
 sudo docker/conf.d/0-base.sh
 sudo docker/conf.d/0-docker.sh
-sudo docker/conf.d/1-go.sh
+sudo -E docker/conf.d/1-go.sh
 sudo cp bin/workon bin/buildenv /usr/local/bin/
 buildenv $1
 
